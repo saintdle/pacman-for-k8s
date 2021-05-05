ece3f1d7c609b05dede0984187b84ab4f031b1a7
Dean Lewis <22192242+saintdle@users.noreply.github.com>
2021-05-05 11:10:08 +0100

    Cleaned up service accounts, added rbac for pod to read node name it runs on to provide more data to user in the Pac-Man UI

--- pacman-install.sh
+++ pacman-install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 kubectl create namespace pacman
-kubectl create -n pacman -f rbac/rbac.yaml
+kubectl create -n pacman -f security/rbac.yaml
+kubectl create -n pacman -f security/secret.yaml
 kubectl create -n pacman -f persistentvolumeclaim/mongo-pvc.yaml
 kubectl create -n pacman -f deployments/mongo-deployment.yaml
 while [ "$(kubectl get pods -l=name='mongo' -n pacman -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true" ]; do
